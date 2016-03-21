5fdf8e3954773f9b74c90d89eb4d409dac773467
wuyuling35 <248232729@qq.com>
2016-03-21 18:13:51 +0800

    1. 第一个工程
    上传工程

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 吴于凌 on 16/3/21.
+//  Copyright © 2016年 吴于凌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
